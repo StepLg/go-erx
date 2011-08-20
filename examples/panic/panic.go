077f936481d0933a0087f5eed9aa5ce02b6e4308
strike <actionstrike@gmail.com>
2011-08-20 18:44:19 +0400

    Corrected examples and thir Makefiles

--- examples/panic/panic.go
+++ examples/panic/panic.go
@@ -29,7 +29,7 @@ func fileSum(fileName string) (result int) {
 	}
 
 	result = 0
-	f, err := os.Open(fileName, os.O_RDONLY, 0000)
+	f, err := os.OpenFile(fileName, os.O_RDONLY, 0000)
 	if f==nil {
 		panic(makeError(erx.NewSequent("Open file.", err)))
 	}
@@ -41,7 +41,7 @@ func fileSum(fileName string) (result int) {
 	for err==nil || err==os.EOF {
 		if strings.TrimSpace(line)!="" {
 			chunkNum := 1
-			for _, chunk := range strings.Split(line, " ", 0) {
+			for _, chunk := range strings.Split(line, " ") {
 				var curInt int
 				curInt, err = strconv.Atoi(strings.TrimSpace(chunk))
 				if err!=nil {
